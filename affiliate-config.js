2709add15342d617ca457942301f7b678ac0c5d6
saad055561-beep <saad055561@gmail.com>
2026-09-06 17:26:56 +0300

    إضافة رابط نون الرسمي لـ Samsung Galaxy A17 4G

--- affiliate-config.js
+++ affiliate-config.js
@@ -7,7 +7,8 @@ window.DABBIRHA_AFFILIATE={
     name:'نون',
     url:'https://www.noon.com/saudi-ar/',
     links:{
-      'Xiaomi Redmi Note 14':'https://s.noon.com/DlPsJs69Auk'
+      'Xiaomi Redmi Note 14':'https://s.noon.com/DlPsJs69Auk',
+      'Samsung Galaxy A17 4G':'https://s.noon.com/o7QQpIWMzbc'
     }
   },
   temu:{enabled:false,name:'Temu',url:'https://www.temu.com/'},
