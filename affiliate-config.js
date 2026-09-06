cdfb92616b7c5ece5eb1940c002829b270e008df
saad055561-beep <saad055561@gmail.com>
2026-09-06 17:50:25 +0300

    Add Noon affiliate links for expanded catalog

--- affiliate-config.js
+++ affiliate-config.js
@@ -9,7 +9,19 @@ window.DABBIRHA_AFFILIATE={
     links:{
       'Xiaomi Redmi Note 14':'https://s.noon.com/DlPsJs69Auk',
       'Samsung Galaxy A17 4G':'https://s.noon.com/o7QQpIWMzbc',
-      'Samsung Galaxy A26 5G':'https://s.noon.com/gKinfuKUgLw'
+      'Samsung Galaxy A26 5G':'https://s.noon.com/gKinfuKUgLw',
+      'Lenovo IdeaPad 1':'https://s.noon.com/ZjwADSOoJug',
+      'HP 15':'https://s.noon.com/73u7Y2_YbvQ',
+      'ASUS Vivobook 15':'https://s.noon.com/ic0Knt8DuvU',
+      'Samsung 24 بوصة':'https://s.noon.com/p2tQALN_VBY',
+      'Samsung 32 بوصة':'https://s.noon.com/l2Cyl2RkQdw',
+      'LG UltraGear 27':'https://s.noon.com/CaueD21c6qI',
+      'Samsung Galaxy Buds FE':'https://s.noon.com/QCoC6_-cr28',
+      'Anker Soundcore':'https://s.noon.com/lt1Vb4jtZZQ',
+      'Sony WH-CH520':'https://s.noon.com/g8XqKRSX684',
+      'Sony WH-1000XM5':'https://s.noon.com/8dlC8stL1e8',
+      'HONOR 400 5G':'https://s.noon.com/WUAWZioMfTo',
+      'iPhone 16e':'https://s.noon.com/OwuVifEEtCU'
     }
   },
   temu:{enabled:false,name:'Temu',url:'https://www.temu.com/'},
